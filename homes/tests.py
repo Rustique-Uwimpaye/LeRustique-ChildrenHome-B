1576c17be57cd335ccdd8a8ccd5cf6437966b3a6
Rustique-Uwimpaye <rustique644@gmail.com>
2020-12-09 12:52:07 +0300

    added all the required tests

--- homes/tests.py
+++ homes/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+
 
 # Create your tests here.
+
+class UserAccountTests(TestCase):
+    
+    def test_new_superuser(self):
+        db = get_user_model()
+        super_user = db.objects.create_superuser('testuser@super.com', 'username', 'firstname', 'lastname', 'password')
+        self.assertEqual(super_user.email, 'testuser@super.com')
+        self.assertEqual(super_user.user_name, 'username')
+        self.assertEqual(super_user.first_name, 'firstname')
+        self.assertEqual(super_user.last_name, 'lastname')
+        self.assertTrue(super_user.is_superuser) 
+        self.assertTrue(super_user.is_staff)
+        self.assertTrue(super_user.is_active)
+        self.assertEqual(str(super_user), 'username')
+        
+        with self.assertRaises(ValueError):
+            db.objects.create_superuser(email='testuser@super.com', user_name='username', first_name='first_name', last_name='last_name', password='password', is_superuser=False)  
+            
+        with self.assertRaises(ValueError):
+            db.objects.create_superuser(email='testuser@super.com', user_name='username', first_name='first_name', last_name='last_name', password='password', is_staff=False)
+            
+        with self.assertRaises(ValueError):
+            db.objects.create_superuser(email='', user_name='username', first_name='first_name', last_name='last_name', password='password', is_superuser=False)   
+            
+    def test_new_user(self):
+        db = get_user_model()
+        user = db.objects.create_user('testuser@user.com', 'username', 'firstname', 'lastname', 'password')
+        self.assertEqual(user.email, 'testuser@user.com')
+        self.assertEqual(user.user_name, 'username')
+        self.assertEqual(user.first_name, 'firstname')  
+        self.assertEqual(user.last_name, 'lastname')
+        self.assertFalse(user.is_superuser)  
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_active)
+        
+        with self.assertRaises(ValueError):
+            db.objects.create_user(email='', user_name='Dean', first_name='first_name', last_name='last_name', password='password')  
